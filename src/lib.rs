976762785b9d7dbf31d453fb979480ac80a2c3fb
David Wallace Croft <david@CroftSoft.com>
2022-12-03 08:48:24 -0600

    rust since

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+// =============================================================================
+//! - CroftSoft Evolve
+//!
+//! # Metadata
+//! - Copyright: &copy; 1996-2022 [`CroftSoft Inc`]
+//! - Author: [`David Wallace Croft`]
+//! - Rust version: 2022-12-03
+//! - Rust since: 2022-09-12
+//! - Java version: 2008-04-19
+//! - Java since: 1996-09-01
+//!
+//! # History
+//! - Adapted from the Java package com.croftsoft.apps.evolve
+//!   - In the Java-based [`CroftSoft Apps Library`]
+//!
+//! [`CroftSoft Apps Library`]: https://www.croftsoft.com/library/code/
+//! [`CroftSoft Inc`]: https://www.croftsoft.com/
+//! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
+// =============================================================================
+
 use js_sys::Object;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
