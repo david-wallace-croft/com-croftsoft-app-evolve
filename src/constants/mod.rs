24cd3eaa9d145b432a3dbd1ab3192c50fb9d61c1
David Wallace Croft <david@CroftSoft.com>
2022-12-25 16:02:43 -0600

    v0.5.1-SNAPSHOT

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -22,7 +22,8 @@ use crate::components::evolve::EvolveComponentInitialConfiguration;
 
 // pub static FRAME_ICON_FILENAME: &str = "/images/david.png";
 // pub static SHUTDOWN_CONFIRMATION_PROMPT: &str = "Close CroftSoft Evolve?";
-pub static INFO: &str = "CroftSoft Evolve v0.5.0 Copyright 2022 CroftSoft Inc";
+pub static INFO: &str =
+  "CroftSoft Evolve v0.5.1-SNAPSHOT Copyright 2022 CroftSoft Inc";
 
 pub const BABY_ENERGY: usize = 10;
 pub const BIRTH_ENERGY: usize = 30;
